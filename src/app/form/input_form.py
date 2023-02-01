56e15df15eee2ee0760b9ac29e9e35b635a9d990
akumaru37 <active.frontier37+akumaru37@gmail.com>
2023-02-02 02:22:26 +0900

    formも

--- src/app/form/input_form.py
+++ src/app/form/input_form.py
@@ -1,10 +1,14 @@
-from django.forms import forms
+from django.core.validators import RegexValidator
+from django.forms import forms, TextInput
 from django.forms.fields import CharField
 
 class InputForm(forms.Form):
-    a = CharField(max_length=30, required=True, error_messages={
-        'required': 'Aを入力してください',
-    })
+    a = CharField(max_length=30,
+                  required=True,
+                  error_messages={'required': 'Aを入力してください'},
+                  validators=[RegexValidator(regex="^[A-Za-z0-9_]+$", message='ああああああ')]
+
+    )
 
     def __init__(self, *arags, **kwargs):
         super().__init__(*arags, **kwargs)
